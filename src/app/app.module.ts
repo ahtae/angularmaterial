7b9dae5251e1498fe43a4badfe3ebbfe86ca304e
Kristy Li <kristyli20.0@gmail.com>
2021-05-05 21:46:27 -0400

    chore: Import modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+const routes: Routes = [
+  { path: 'demo', loadChildren: () => import('./demo/demo.module').then(m => m.DemoModule)},
+  { path: '**', redirectTo: 'demo'}
+];
 
 @NgModule({
   declarations: [
@@ -10,7 +16,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
